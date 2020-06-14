3efdbe6a18eb8bc808dc005b48e3ba758e3fc7e3
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2020-06-14 15:55:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -36,4 +36,13 @@ var respecConfig = {
   github: "https://github.com/BROprogramma/BHR-G",
   previousPublishDate: "2019-09-30",
   previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
  };
