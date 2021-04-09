4dd19a8e57808cf3543beefd37b4f0995a5570ab
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2021-04-09 12:35:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var respecConfig = {
-  specStatus: "GN-DEF",
+  specStatus: "GN-WV",
   specType: "IM",
   subtitle: "Geologische boormonsterbeschrijving en boormonsteranalyse",
   //canonicalURI: "WV",
-  publishDate: "2021-04-08",
+  //publishDate: "2021-04-08",
   maxTocLevel: 4,
  logos: [{
     src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
@@ -27,6 +27,6 @@ var respecConfig = {
   shortName: "bhr-g",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/BHR-G",
-  previousPublishDate: "20202-03-25",
+  previousPublishDate: "2021-04-08",
   previousStatus: "GN-DEF",
 };
