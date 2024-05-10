313f46cad54428658483d485c308703e08dc4f3c
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2024-05-10 12:33:47 +0200

    Update config.js
    versie ter consulatie

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@ let respecConfig = {
   title: "Basisregistratie Ondergrond Catalogus Geologische boormonsterbeschrijving en boormonsteranalyse",
   subtitle: "Versie 3.19",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
-  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "wv",                   // Werkversie
+  specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "bhr-g",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-04-12",
+  publishDate: "2024-05-10",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
