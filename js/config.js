a3e16fcb545ccde1353de57dd5c17b71af1ddd78
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2024-10-02 10:50:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ let respecConfig = {
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Basisregistratie Ondergrond Catalogus Booronderzoek",
-  subtitle: "Geologische boormonsterbeschrijving en boormonsteranalyse" + " 3.20",
+  subtitle: "Geologische boormonsterbeschrijving en boormonsteranalyse 3.20",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
