dcc76e502a6a08405224e8585c21a0bd66f2984d
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2024-10-04 15:45:16 +0200

    Update config.js
    title aangepast

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Basisregistratie Ondergrond Catalogus Geologisch Booronderzoek",
+  title: "Basisregistratie Ondergrond Catalogus Geologisch booronderzoek",
   subtitle: "Versie 3.2",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
