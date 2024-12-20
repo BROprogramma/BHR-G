2c0cf1b36f6e65cba51d2fe8b663cc05c745e75f
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2024-12-20 06:58:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,9 @@ let respecConfig = {
   title: "Basisregistratie Ondergrond Catalogus Booronderzoek - Geologische boormonsterbeschrijving en boormonsteranalyse",
   subtitle: "Versie 3.2",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "bhr-g",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-05-10",
+  publishDate: "202x-xx-xx",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
