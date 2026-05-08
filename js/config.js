f459531739b6979968d5c9b6020918744041932e
Jacob Vos <jacobvos@impronotion.nl>
2026-05-08 11:37:14 +0200

    edDraftURI met mnemonic in uppercase

--- js/config.js
+++ js/config.js
@@ -76,7 +76,7 @@ let respecConfig = {
       },
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
-  edDraftURI: "https://broprogramma.github.io/bhr-g/",
+  edDraftURI: "https://broprogramma.github.io/BHR-G/",
   //github: "https://github.com/BROprogramma/BHR-G",
   postProcess: [
     ...(typeof organisationConfig !== "undefined" &&
