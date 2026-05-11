74948d23d63418d6793665205c438f7a9ae627ff
JanWiersma <123484801+JanWiersma@users.noreply.github.com>
2026-05-11 15:35:28 +0200

    Update specStatus and publishDate in config.js

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@ let respecConfig = {
   title: "Catalogus registratie ondergrond Booronderzoek - Geologische boormonsterbeschrijving en boormonsteranalyse",
   subtitle: "Versie 3.21",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
+  specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "bhr-g",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2026-05-11",
+  publishDate: "2026-05-11",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: " ",
